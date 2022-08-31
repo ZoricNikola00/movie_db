6c4926c3305cbdd1ba06cb7946bbaa3dd2b6f16f
ZoricNikola00 <zoriclfc@gmail.com>
2022-08-31 13:54:16 +0200

    adding React Query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context';
+import {QueryClientProvider,QueryClient} from '@tanstack/react-query'
 
+const queryClient=new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
+  <QueryClientProvider client={queryClient}>
+      <AppProvider>
         <App />
       </AppProvider>
+  </QueryClientProvider>
   </React.StrictMode>
 );
