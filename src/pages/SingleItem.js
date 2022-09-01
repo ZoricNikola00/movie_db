374e7b21faa130884bbc051c741d46f27a1ac257
ZoricNikola00 <zoriclfc@gmail.com>
2022-09-02 01:15:30 +0200

    Minor fixes in SingleItem page

--- src/pages/SingleItem.js
+++ src/pages/SingleItem.js
@@ -2,7 +2,7 @@ import { useParams,Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
 import { useQuery } from "@tanstack/react-query";
 import ReactLoading from 'react-loading';
-import {FaImdb} from 'react-icons/fa'
+import {FaImdb,FaLink} from 'react-icons/fa'
 const SingleItem = () => {
     const {id,type}=useParams()
     const {fetchData}=useGlobalContext()
@@ -17,8 +17,10 @@ const SingleItem = () => {
     if(isError){
         return <div>{error}</div>
     }
-
-    const {budget,revenue,title,homepage,status,vote_average,backdrop_path,genres,original_language,tagline,runtime,release_date,poster_path,overview,original_title}=data
+    console.log(data)
+    const {budget,revenue,title,homepage,name,status,vote_average,first_air_date,backdrop_path,last_air_date,genres,original_language,tagline,runtime,release_date,poster_path,overview,original_title}=data
+    const titleCorrect=type==='tv'?name:title
+    const date=release_date && type==='movie'?release_date.slice(0,4): first_air_date && first_air_date.slice(0,4)!==last_air_date.slice(0,4)?`${first_air_date.slice(0,4)}-${last_air_date.slice(0,4)}`:`${first_air_date && first_air_date.slice(0,4)}-`
     const ratingColor={
         border:vote_average>6.99?'green 4px solid':vote_average>3.99?'yellow 4px solid':'red 4px solid'
     }
@@ -30,13 +32,13 @@ const SingleItem = () => {
                 <img src={`${img_path}${poster_path}`}/>
             </div>
             <div className="overview">
-                <div className="title"><h1>{title}</h1><small>({release_date.slice(0,4)})</small></div>
+                <div className="title"><h1>{titleCorrect}</h1><small>({date})</small><a className="homepage" href={homepage}><FaLink/></a></div>
                 <div className="infoItem">
                     <div className="genres">
                         {genres.map(x=><p key={x.id}>{x.name}</p>)}
                     </div>
-                    <div className="dot"></div>
-                    <p>{runtime}min</p>
+                    {type==='movie' && <div className="dot"></div>}
+                    {type==='movie' && <p>{runtime}min</p>}
                 </div>
                 <div className="ratings">
                     <div style={ratingColor} className='voteScore single'>{(vote_average*10).toFixed()}</div>
@@ -62,12 +64,12 @@ const SingleItem = () => {
             </div>
             <div className="additionalInfo">
                 <div >
-                    <FaImdb/>
+                    <FaImdb className="imdbSingle"/>
                 </div>
-                <div>
+                {original_title &&<div>
                     <p>Original Title</p>
                     <p>{original_title}</p>
-                </div>
+                </div>}
                 <div>
                     <p>Status</p>
                     <p>{status}</p>
