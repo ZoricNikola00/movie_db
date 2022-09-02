3b1d315ca85220830c71875b1d4cf5d14f1fe29d
ZoricNikola00 <zoriclfc@gmail.com>
2022-09-02 02:20:41 +0200

    Home page fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,12 +9,12 @@ const Home = () => {
           <img className="imgHome" src={img}/>
           <h1 className="homeTitle">Welcome To The MovieDB</h1>
           <SearchBar/>
-          <NowPlaying/>
         </div>  
+        <h2>Now In Theater...</h2>
+          <NowPlaying/>
       </div>
   )
-  //https://images.unsplash.com/photo-1488866022504-f2584929ca5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmlnaHR8ZW58MHx8MHx8&w=1000&q=80
-  //https://api.themoviedb.org/3/movie/now_playing?api_key=72de8895bb64376912ef844faac64a10&page=
+ 
 }
 
 export default Home
