66765c1e21badd6136efbf466bebd293a493dff5
ZoricNikola00 <zoriclfc@gmail.com>
2022-09-03 22:35:17 +0200

    Creating About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <h1>About</h1>
+      <p>Ipsum enim occaecat consectetur do nulla in ullamco officia tempor est minim qui esse. Culpa velit deserunt incididunt eiusmod magna pariatur enim non amet laborum. Do elit irure minim ea incididunt ipsum sit pariatur excepteur enim qui dolor dolore dolor. Sint amet incididunt ut id aliqua dolore exercitation aliqua dolor. Mollit eiusmod aliquip dolore deserunt ipsum tempor. Commodo laboris magna sint sunt eiusmod.</p>
+    </div>
   )
 }
 
